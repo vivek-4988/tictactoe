246b915a27a3f252c84d743e3962759a9685591a
vivek <vivek@creatingwinners.com>
2022-02-06 17:25:55 +0800

    design , style added

--- app/src/main/java/com/vivek/tictactoe/MainActivity.kt
+++ app/src/main/java/com/vivek/tictactoe/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.vivek.tictactoe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.vivek.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var board: Array<Array<Button>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
